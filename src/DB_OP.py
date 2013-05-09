ba69963635c61ae3fdababc4d69d783a1f6b9519
Sai Gopal <lrsaigopalg@gmail.com>
2013-05-10 02:13:58 +0530

    Update Connection Function

--- src/DB_OP.py
+++ src/DB_OP.py
@@ -66,9 +66,8 @@ def details(z,counter):
 def con():
 	from pymongo import MongoClient
 	client=MongoClient("localhost",27017)
-	db = client['test_database']
-	collection = db['test_collection']
-	return db,collection
+	db = client['findb']
+	return db
 
 def insert(z):
 
